b01e4e7d1e60271ccbe246ebf9d5ef0d8c6e4884
Dereck Sherrer <derecksherrer@yahoo.ca>
2020-10-20 19:58:17 -0400

    Init commit - very basic functions

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # calculator
-Calculator
+Calculator app for Odin Project
---
+++ scripts.js
@@ -0,0 +1,24 @@
+let first = parseInt(prompt("First number: "));
+let second = parseInt(prompt("Second number: "));
+
+function add() {
+    return(first + second);
+}
+
+function subtract() {
+    return first - second;
+}
+
+function multiply() {
+    return first * second;
+}
+
+function divide() {
+    return first / second;
+}
+
+function operate() {
+    
+}
+
+console.log(add(first, second));
