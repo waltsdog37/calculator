738a913b3d71e45ebd72fc22e92fc7b701a7d12c
Dereck Sherrer <derecksherrer@yahoo.ca>
2020-11-01 14:25:50 -0500

    Begun adding second css

--- scripts.js
+++ scripts.js
@@ -11,3 +11,19 @@ function operate () {
     answer = firstNumber + operation + secondNumber;
     return answer;
 }
+
+let style = document.getElementById("style");
+console.log(style.href);
+
+function changeStyle () {
+    let style = document.getElementById("style");
+    console.log(style.href);
+    if (style.href = "ios.css") {
+        style.href = "oldschool.css";
+        console.log("yes");
+    } else {
+        style.href = "ios.css";
+        console.log("no");
+    }
+
+}
