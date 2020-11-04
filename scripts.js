f7b3beee3abf566be0fd39eef96fa748f1ae3126
Dereck Sherrer <derecksherrer@yahoo.ca>
2020-11-04 07:51:59 -0500

    Init start to button logic and flow

--- scripts.js
+++ scripts.js
@@ -1,21 +1,83 @@
-function firstNumber () {
+let number = [];
+let display;
 
+// listen for number buttons to be clicked and add to number array
+let btns = document.querySelectorAll(".number, .function");
+for (let i=0; i<btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        number.push(btns[i].value);  
+        console.log(number);
+        output(number);
+    });
 }
 
-function secondNumber () {
-
+// display number, as buttons pushed, in output area
+// clear output if "a/c" pushed
+function output(number) {
+    display = document.getElementById("output")
+    display.innerHTML = number.join("");
+    if (number[number.length-1] == '') {
+        display.value = "";
+    }
 }
 
 
-function operate () {
-    answer = firstNumber + operation + secondNumber;
+function getOperation(element) {
+    let operation;
+    if (element == "plus-ios" || element == "plus-old") {
+        operation = "+";
+    } else {
+        operation = element;
+    }
+    operate(operation);
+}
+
+function operate(operation) {
+    switch (operation) {
+        case "+":
+            add();
+            break;
+        case "-":
+            console.log("sub ")
+            subtract();
+            break;
+        case "*":
+            multiply();
+            break;
+        case "/":
+            divide();
+            break;
+    }
+}
+
+// Four main operation functions
+function add(first, second) {
+    let answer = first + second;
+    console.log(answer);
+}
+
+function subtract(first, second) {
+    console.log(first, second);
+    let answer = first - second;
+    console.log(answer);
+}
+
+function multiply(first, second) {
+    let answer = first * second;
     return answer;
 }
 
-let style = document.getElementById("style");
-console.log(style.href);
+function divide(first, second) {
+    let answer = first / second;
+    return answer;
+}
 
-function changeStyle () {
+
+
+
+
+// change CSS file with click of toggle switch
+function changeStyle() {
     let style = document.getElementById("style");
     let old = "oldschool.css";
     let ios = "ios.css";
@@ -26,3 +88,4 @@ function changeStyle () {
         style.setAttribute("href", ios);
     }
 }
+
